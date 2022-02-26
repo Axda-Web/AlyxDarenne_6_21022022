fa87deeb2a45c495dd0448d73fb9ac684a4697b0
Axda <axda.web@gmail.com>
2022-02-26 10:38:01 +0100

    Fetch & display data in photographer.html media grid + bottom orange fixed box

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -7,19 +7,25 @@ const urlParams = new URLSearchParams(queryString)
 const photographerId = urlParams.get('photographerid')
 
 
-const getCurrentPhotographerData = async () => {
+const manageCurrentPhotographerData = async () => {
 
     //Récupérer les données du photographe via son ID
     const jsonData = await fetch('../../data/photographers.json')
     const data = await jsonData.json()
     const currentPhotographerData = data.photographers.find(item => item.id == photographerId)
     const {name, city, country, tagline, price, portrait} = currentPhotographerData
-    const imgUrl = `assets/photographers/${portrait}`
+
+    //Récupérer les données media associées au photographe
+    const currentPhotographerMedia = data.media.filter(item => item.photographerId == photographerId)
+
+    //Récupérer le nombre total de like du photograph
+    let allLikes = currentPhotographerMedia.map(item => item.likes).reduce((total, value) => total + value)
     
     
     //Mise en page des données du photographe
-    const displayData = () => {
+    const displayHeaderData = () => {
 
+    const imgUrl = `assets/photographers/${portrait}`
     const photographHeader = document.querySelector('.photograph-header')
 
     const photographDescription = document.createElement('div')
@@ -41,21 +47,59 @@ const getCurrentPhotographerData = async () => {
     photographTagline.classList.add('photograph-tagline')
     photographDescription.appendChild(photographTagline)
 
-    // const photographPrice = document.createElement('p')
-    // photographPrice.innerText = `${price}€/jour`
-    // photographPrice.classList.add('photograph-price')
-    // photographDescription.appendChild(photographPrice)
-
     const photographImg = document.createElement('img')
     photographImg.src = imgUrl
     photographImg.classList.add('photograph-img')
     photographHeader.appendChild(photographImg)
     }
 
-    displayData()
+    displayHeaderData()
+
+    const displayMediaGrid = () => {
+
+        const mediaGridContainer = document.querySelector('.media-grid')
+        
+        currentPhotographerMedia.map(item => {
+
+            //Vérification du type de media (part 1)
+            const mediaPicture = !item.image ? `/assets/SamplePhotos/${item.photographerId}/${item.video}` : `/assets/SamplePhotos/${item.photographerId}/${item.image}`
+            const imgElt = `<img class="media-item__img" src=${mediaPicture} alt=${item.title} />`
+            const videoElt = `<video class="media-item__video" width="300" height="280" >
+                                <source src=${mediaPicture} type="video/mp4">
+                                Your browser does not support the video tag.
+                            </video>`
+
+            return mediaGridContainer.innerHTML += `
+            <article class="media-item">
+                <div class="media-item__media-container">
+                    ${!item.image ? videoElt : imgElt /* Vérification du type de media (part 2)*/}
+                </div>
+                <div class="media-item__text-container">
+                    <p class="media-item__title">${item.title}</p>
+                    <p class="media-item__likes">${item.likes} <i class="fas fa-heart"></i></p>
+                </div>
+            </article>`
+    })
+    
+
+    }
+
+    displayMediaGrid()
+
+
+    const  displayFixedBottomBlock = () => {
+        const orangeBlock = document.createElement('aside')
+        orangeBlock.classList.add('orange-box')
+        orangeBlock.innerHTML = `<p>${allLikes} <i class="fas fa-heart"></i></p>
+                                <p>${price}€ / jour</p>`
+        document.body.appendChild(orangeBlock)
+    }
+
+    displayFixedBottomBlock()
+
 }
 
-getCurrentPhotographerData()
+manageCurrentPhotographerData()
 
 
 
